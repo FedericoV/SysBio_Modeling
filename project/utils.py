b422a83af8c563af7ead94cdbc6b04f79515e960
Federico Vaggi <vaggi.federico@gmail.com>
2014-05-07 16:44:37 +0200

    Added mapping functions for common scenarios.

---
+++ project/utils.py
@@ -0,0 +1,54 @@
+__author__ = 'Federico Vaggi'
+
+import numpy as np
+
+
+def direct_model_var_to_measure(model_sim, model_timepoints, experiment, measurement, mapping_parameters):
+    model_idx = mapping_parameters
+    _, _, measure_timepoints = measurement.get_nonzero_measurements()
+    exp_t_idx = np.searchsorted(model_timepoints, measure_timepoints)
+    return model_sim[exp_t_idx, model_idx], exp_t_idx
+
+
+def direct_model_jac_to_measure_jac(model_jacobian, model_timepoints, experiment, measurement, mapping_parameters):
+    model_idx = mapping_parameters
+    n_exp_params = len(experiment.param_global_vector_idx)
+    _, _, measure_timepoints = measurement.get_nonzero_measurements()
+    exp_t_idx = np.searchsorted(model_timepoints, measure_timepoints)
+
+    # Mapping between experimental measurement and model variable
+    v = model_idx * n_exp_params
+
+    return model_jacobian[exp_t_idx, v:(v + n_exp_params)]
+
+
+def sum_model_vars_to_measure(model_sim, model_timepoints, experiment, measurement, mapping_parameters):
+    model_variable_idxs = mapping_parameters
+    _, _, measure_timepoints = measurement.get_nonzero_measurements()
+    exp_t_idx = np.searchsorted(model_timepoints, measure_timepoints)
+    n_model_vars = measurement.variable_name
+
+    measure_sim = np.zeros((len(exp_t_idx), n_model_vars))
+    for v in model_variable_idxs:
+        measure_sim += model_sim[exp_t_idx, v]
+
+    return measure_sim, exp_t_idx
+
+
+def sum_model_jac_to_measure_jac(model_jacobian, model_timepoints, experiment, measurement, mapping_parameters):
+    model_variable_idxs = mapping_parameters
+    n_exp_params = len(experiment.param_global_vector_idx)
+    _, _, measure_timepoints = measurement.get_nonzero_measurements()
+    exp_t_idx = np.searchsorted(model_timepoints, measure_timepoints)
+    n_model_vars = measurement.variable_name
+
+    measure_jac = np.zeros((len(exp_t_idx), n_model_vars))
+    for v in model_variable_idxs:
+        v *= n_exp_params
+        measure_jac += model_jacobian[exp_t_idx, v:(v + n_exp_params)]
+
+    return measure_jac
+
+
+
+
