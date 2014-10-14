2264be95d78f02129b6434711f0132baede376dc
Federico Vaggi <vaggi.federico@gmail.com>
2014-10-14 13:26:40 +0200

    Raises error if any values of std are 0

--- measurement/abstract_measurement.py
+++ measurement/abstract_measurement.py
@@ -1,6 +1,7 @@
 __author__ = 'Federico Vaggi'
 
-from abc import ABCMeta, abstractmethod, abstractproperty
+from abc import ABCMeta, abstractmethod
+
 import numpy as np
 
 
@@ -11,6 +12,9 @@ class MeasurementABC(object):
     def __init__(self, variable_name, measurement_value, measurement_std=None):
         if measurement_std is None:
             measurement_std = np.ones_like(measurement_value)
+
+        if np.any(measurement_std == 0):
+            raise ValueError('Standard deviation of measurement cannot be 0')
 
         if not (len(measurement_value) == len(measurement_std)):
             raise ValueError('Length of Standard Deviation Array Not Equal to Length of Measurements')
